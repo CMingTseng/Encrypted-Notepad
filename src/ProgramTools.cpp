e49977c4f628da224b6d5884330f6841869b7da6
oxygen-TW <howard2805@gmail.com>
2018-04-22 18:03:13 +0800

    Add SHA512Tools

---
+++ src/ProgramTools.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+std::string SHA512(char* string){
+    unsigned char digest[SHA512_DIGEST_LENGTH];
+
+    SHA512((unsigned char*)&string, strlen(string), (unsigned char*)&digest);   
+
+    char mdString[SHA512_DIGEST_LENGTH*2+1];
+
+    for(int i = 0; i < SHA512_DIGEST_LENGTH; i++)
+         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+
+    //printf("SHA512 digest: %s\n", mdString);
+    std::string SHA512str(mdString);
+    return SHA512str;
+}
